ac92a5df9bdf0eccfd5a057bede39197ec577c81
Hussein <31306960+flamingbolt2@users.noreply.github.com>
2017-08-29 11:15:22 +0200

    Lektion2 Car

---
+++ src/Lektion2/Car.java
@@ -0,0 +1,14 @@
+package Lektion2;
+
+public class Car {
+     
+	
+	 
+  public static void main(String[] args) {
+	  int 	ANTALET RATTAR =4 ;
+			  
+	 System.out.println(ANTALET_RATTAR):
+
+	}
+
+}
